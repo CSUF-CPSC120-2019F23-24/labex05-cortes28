ae71ff4376dc3f37bd7e6c1366de29b52a08b0bc
cortes28 <cortes28@csu.fullerton.edu>
2019-09-21 14:12:04 -0700

    made a random formula for a rectangle

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -1,1 +1,35 @@
 // Random Rectangle
+
+  #include <iostream>
+  #include <iomanip>
+  #include <ctime>
+  #include <random>
+  #include <string>
+
+  int main()
+{
+    // I are setting a constant which will have height as 3 always no matter
+    // the length of the rectangle which is limited having a base and length of
+    // 40.
+    const int HEIGHT = 3;
+
+    srand(time(0));
+    int max = 40;
+    int length;
+    length = rand() % max + 1;
+
+    // Assigned the length to a character/
+    std::string rectangle;
+    rectangle.assign(length,'#');
+    rectangle.assign(length,'#');
+    rectangle.assign(length,'#');
+
+    //displayed the output of our function to the program for the user to see.
+
+    std::cout << rectangle << std::endl;
+    std::cout << rectangle << std::endl;
+    std::cout << rectangle << std::endl;
+
+
+    return 0;
+}
